0ec777a4e4663548d861b57128b5d491a9c1d211
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2024-09-14 15:17:29 -0400

    try to fix

--- app/nfc/adapter.jsx
+++ app/nfc/adapter.jsx
@@ -6,13 +6,15 @@ export async function readNFCTag() {
             NfcManager.requestTechnology(NfcTech.NfcA).then(() => {
                 NfcManager.getTag().then((tag) => {
                     const message = tag.ndefMessage[0].payload;
-                    console.log('Received NFC data:', NfcManager.ndef.textDecoder(message));
+                    console.log('Received NFC data:', NfcManager.nfcAHandler.textDecoder(message));
                 }).catch((err) => {
                     console.log(err);
                 }).finally(() => {
-                    // NfcManager.cancelTechnologyRequest();
+                    NfcManager.cancelTechnologyRequest();
                 });
-            })
+            }).catch((err) => {
+                console.log(err);
+            });
         }
     })
 };
@@ -20,7 +22,7 @@ export async function readNFCTag() {
 export async function writeNFCTag(text) {
     try {
         await NfcManager.requestTechnology(NfcTech.NfcA);
-        await NfcManager.writeNdefMessage([NfcManager.ndef.textRecord(text)]);
+        await NfcManager.nfcAHandler.writeNdefMessage([NfcManager.nfcAHandler.textRecord(text)]);
         console.log('Successfully wrote to tag');
     } catch (ex) {
         console.warn('Error writing to tag', ex);
