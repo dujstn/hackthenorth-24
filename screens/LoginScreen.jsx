f2d2d0cffbbcb4a84b0b30cb7d92fc18c88a2b7b
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2024-09-15 03:09:38 -0400

    remove auth0

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { useAuth0 } from 'react-native-auth0';
 
 const LoginScreen = ({ navigation }) => {
-  const { authorize } = useAuth0();
 
   const login = async () => {
     try {
-      await authorize();
       navigation.navigate('Home');
     } catch (error) {
       console.log(error);
