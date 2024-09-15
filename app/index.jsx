73c29da1347f2b0f12bf5d5dc94ff0b157f17731
Evan Su <48808396+HACKERALERT@users.noreply.github.com>
2024-09-15 02:37:19 -0400

    no auth0

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,15 @@
-import {Auth0Provider} from 'react-native-auth0';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import LoginScreen from '../screens/LoginScreen';
 import NewProfileScreen from '../screens/NewProfileScreen';
 import SharingScreen from '../app/nfc/index';
-import config from '../auth0-configuration'
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <Auth0Provider domain={config.domain}
-                   clientId={config.clientId}>
       <AppNavigator/>
-    </Auth0Provider>
   );
 }
 
